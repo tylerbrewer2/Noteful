a4d865a339aac1b8164dcae044c2438c940f16e7
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-06-30 20:46:39 +0000

    These are a bunch of changes

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,5 +5,8 @@ class Note < ActiveRecord::Base
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
-
+  
+  def preview
+    self.content.slice(0..100).concat "...(readmore)"
+  end
 end
